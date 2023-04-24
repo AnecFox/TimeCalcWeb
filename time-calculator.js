ca15f8a8a01d0863d01957add93f011875c122cf
Anec <anecfox@gmail.com>
2023-04-24 20:33:01 +0300

    Fixed encoding and replaced == to === in time-calculator.js

--- time-calculator.js
+++ time-calculator.js
@@ -3,7 +3,7 @@ let secondTime = document.getElementById("second-time");
 let result = document.getElementById("result");
 
 function calculateTime() {
-    if (firstTime.value == "" || secondTime.value == "") {
+    if (firstTime.value === "" || secondTime.value === "") {
         result.innerHTML = "Время не выбрано";
     } else {
         let firstTimeValue = new Date("April 23, 2023 " + firstTime.value);
@@ -21,11 +21,11 @@ function calculateTime() {
 
         result.innerHTML = "Результат: ";
 
-        if (hours != 0 && minutes != 0) {
+        if (hours !== 0 && minutes !== 0) {
             result.innerHTML += hours + " " + hourWord + ", " + minutes + " " + minuteWord;
         } else {
-            result.innerHTML += (hours == 0 ? "" : hours + " " + hourWord + " ") +
-                (minutes == 0 && hours != 0 ? "" : minutes + " " + minuteWord);
+            result.innerHTML += (hours === 0 ? "" : hours + " " + hourWord + " ") +
+                (minutes === 0 && hours !== 0 ? "" : minutes + " " + minuteWord);
         }
     }
 }
@@ -34,7 +34,7 @@ function selectWordForNumber(number, words) {
     let numberSymbols = number.toString();
     let lastNumberSymbol = parseInt(numberSymbols[numberSymbols.length - 1]);
 
-    if (number.toString().endsWith("1") && number != 11) {
+    if (number.toString().endsWith("1") && number !== 11) {
         return words[0];
     } else if (lastNumberSymbol <= 4 && lastNumberSymbol >= 2 && !(number > 11 && number < 15)) {
         return words[1];
